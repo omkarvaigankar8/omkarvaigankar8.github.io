e1018e75781bbba05a5e7bf526959c3bdbddd46d
Omkar Vaigankar <42296321+omkarvaigankar8@users.noreply.github.com>
2020-05-31 23:39:06 +0530

    weather app

---
+++ weather-app/scripts/forecast.js
@@ -0,0 +1,19 @@
+const key ='8zWQt8DgppmWPGsnKmGuzD14T6Zya8dd';
+//get weather information
+const getWeather =async(id) =>{
+    const base='http://dataservice.accuweather.com/currentconditions/v1/';
+    const query=`${id}?apikey=${key}`;
+    const response= await fetch(base+query);
+    const data= await response.json();
+    return data[0];
+};
+
+//getCity information
+const getCity= async(city)=>{
+    const base='http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query =`?apikey=${key}&q=${city}`;
+    const response = await fetch(base+query);
+    const data= await response.json();
+    return data[0];
+};
+ 
